8c581c84750f06d56d8ed095be6cbaa7391c9ab1
Nur Tsalitsna Imamah <nurtsalitsna05@gmail.com>
2023-05-03 21:06:21 +0700

    seat

--- src/main/java/com/spring/binar/challenge_5/repos/SeatRepository.java
+++ src/main/java/com/spring/binar/challenge_5/repos/SeatRepository.java
@@ -1,2 +1,10 @@
-package com.spring.binar.challenge_5.repos;public interface SeatRepository {
+package com.spring.binar.challenge_5.repos;
+
+import com.spring.binar.challenge_5.models.Seat;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+@Repository
+public interface SeatRepository extends JpaRepository <Seat, Integer>{
 }
