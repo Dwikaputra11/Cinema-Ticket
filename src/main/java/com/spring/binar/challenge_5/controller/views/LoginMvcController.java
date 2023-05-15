784dd423256b8c09508313a7e3da891ec84507cb
Abid Bilal <abiddddbilal@gmail.com>
2023-05-15 15:14:10 +0700

    add login and register template

---
+++ src/main/java/com/spring/binar/challenge_5/controller/views/LoginMvcController.java
@@ -0,0 +1,10 @@
+package com.spring.binar.challenge_5.controller.views;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "login")
+public class LoginMvcController {
+    
+}
